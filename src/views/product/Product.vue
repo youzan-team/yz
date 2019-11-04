569afb29104a2de1f05b426097fb59630aa545a2
lehuan <1656008989@qq.com>
2019-11-04 22:47:40 +0800

    1.0

--- src/views/product/Product.vue
+++ src/views/product/Product.vue
@@ -1,15 +1,41 @@
 <template>
-    <div>
-        product
+    <div class="product_body">
+        <!-- <shopNav></shopNav> -->
+        <div class="p_title">
+            <span>商品管理</span>
+            <i>图标</i>
+            <i>?</i>
+            <span>帮助和服务>></span>
+        </div>
+
     </div>
 </template>
 
 <script>
+// import shopNav from "@/components/ShopNav.vue"
 export default {
-
+    name:'shopNav',
+    components: {
+        // shopNav
+    }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.product_body{
+    position:fixed;
+    left:100px;
+    top:0;
+    right:0;
+    bottom:0;
+    background:rgb(247,248,250);
+    overflow:scroll;
+    .p_title{
+        width:100%;
+        height: 56px;
+        background:whrite;
 
+    }
+
+}
 </style>
