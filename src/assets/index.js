b4b6e5083f4ed78c42c0f88a6bb0ccfa3e09dba8
paizhang <1164180295@qq.com>
2019-11-05 12:48:06 +0800

    create 19-11-5

---
+++ src/assets/index.js
@@ -0,0 +1,3 @@
+export const icons ={
+    asrc:'https://img.yzcdn.cn/upload_files/2019/11/05/FsXtsnrT7NdUs9OmCWwOuDClsP2_.png'
+}
---
+++ src/views/shop/Widget.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="widget">
+    <el-row  class="row-header">
+        <el-col :span="12"><div class="">实时概况</div></el-col>
+    </el-row>
+
+   <el-row  class="row-main">
+        <el-col :span="12"><div class="">2</div></el-col>
+        <el-col :span="12"><div class="">23</div></el-col>
+    </el-row>
+
+</div>
+    
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+    .widget{
+         height: 280px;
+         >.row-header{
+            background: #efccce;
+         }
+         >.row-main{
+             background: #0fce0f;
+         }
+    }
+</style>
+
