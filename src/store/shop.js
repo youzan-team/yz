fae8b9157e94fad8ce9dd55d8ae859f5cbc42818
paizhang <1164180295@qq.com>
2019-11-06 22:43:53 +0800

    create 19-11-6

--- src/store/shop.js
+++ src/store/shop.js
@@ -1,13 +1,40 @@
+ import axios from 'axios'
+ 
+function fetch(api,callback) {
+     axios({
+       method:'GET',
+       url:'http://localhost:8080'+api
+     }).then( res => {
+       let data = null;
+       if(res.data.err === 0) {
+         data = res.data.data
+         callback && callback(data)
+       }
+     }).catch( err => {
+        console.log('接口请求异常', err)
+     }).then(() => {
+
+     })
+}
 
 const shop={
+    namespaced: true,
     state:{
-     
+     newsArr:[]
     },
     mutations:{
-      
+      updateNewsArr(state,payload){
+        state.newsArr= payload
+
+      }
     },
     actions:{
-   
+       getNewsArr(store) {
+         fetch('/db/news.json',data => {
+          console.log(data)
+          store.commit('updateNewsArr', data)
+         })
+       }
     }
   }
   
